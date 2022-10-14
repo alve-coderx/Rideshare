9bd6bb223b15b8d45ee1b8d5b365fb39ad707413
alve-coderx <shadmanzz656@gmail.com>
2022-10-13 22:05:09 -0600

    LAaA

--- src/Pages/Rentals/RentalOverView/LandlordOverView.jsx
+++ src/Pages/Rentals/RentalOverView/LandlordOverView.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import { AiOutlineArrowRight, AiOutlineInfoCircle } from 'react-icons/ai'
-import { BiImages } from 'react-icons/bi'
-import { MdDateRange } from 'react-icons/md';
-import { RiCheckboxBlankCircleLine } from 'react-icons/ri'
+import React from 'react';
+import { AiOutlineArrowRight } from 'react-icons/ai';
 import avater from '../../../assets/avater.png';
 
 const LandlordOverView = () => {
     const style = {
-        inputNm: 'p-3 bg-white rounded-xl shadow-2xl font-[500] mt-2 block w-full text-[12px] dark:placeholder-[black]'
+        inputNm: 'p-3 bg-white rounded-xl shadow-2xl font-[500] mt-2 block w-full text-[12px] dark:placeholder-[black]',
+        span  : 'absolute right-2 top-3',
+        primary : 'text-[12px] text-[#AAAFB6] text-[400] flex items-center rounded-lg',
+        secondary : 'text-[13px] font-[600]',
+        absolute : 'flex items-center text-[10px] font-[600]'
     }
     return (
         <div>
@@ -19,20 +20,20 @@ const LandlordOverView = () => {
                     <img className='w-[50px] h-[50px]' src={avater} />
                     <div className='ml-5'>
                         <p className='font-[900] '>Auto Name</p>
-                        <p className='text-[12px] text-[#AAAFB6] text-[400] flex items-center rounded-lg'>5. April 2022 um 12:00  6.April 2022 um 12:00</p>
+                        <p className={style.primary}>5. April 2022 um 12:00  6.April 2022 um 12:00</p>
                         <span className='flex items-center text-[#AAAFB6] text-[10px] font-[600]  absolute right-3 top-4'>WIEs4718</span>
                     </div>
                 </div>
             </div>
             <div className='mx-5 mt-5 mb-4'>
-                <p className='text-[13px] font-[600]'>Preis</p>
+                <p className={style.secondary}>Preis</p>
                 <div className='relative'>
                     <input id="message" rows="4" className={style.inputNm} placeholder="1 Tage (incl. 200km)" />
-                    <span className='absolute right-2 top-3'>15 €</span>
+                    <span className={style.span}>15 €</span>
                 </div>
                 <div className='relative'>
                     <input id="message" rows="4" className={style.inputNm} placeholder="Servicegebühr (25%)" />
-                    <span className='absolute right-2 top-3'>-3,75 €</span>
+                    <span className={style.span}>-3,75 €</span>
                 </div>
                 <div className='relative'>
                     <input id="message" rows="4" className="p-3 bg-white rounded-xl shadow-2xl mt-2 block w-full text-[13px] font-[600] rounded-lg dark:placeholder-[black] " placeholder="Deine Einnahmen" />
@@ -40,34 +41,34 @@ const LandlordOverView = () => {
                 </div>
             </div>
             <div className='mx-5'>
-                <p className='text-[13px] font-[600]'>Abgeschlossene Anmietung</p>
-                <p className='text-[12px] text-[#AAAFB6] text-[400] flex items-center rounded-lg'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nisi, eget quam quis a. Est vivamus semper gravida</p>
+                <p className={style.secondary}>Abgeschlossene Anmietung</p>
+                <p className={style.primary}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nisi, eget quam quis a. Est vivamus semper gravida</p>
             </div>
             <div className='mx-5 mt-5'>
-                <p className='text-[13px] font-[600]'>Oft genutzte Funktionen</p>
+                <p className={style.secondary}>Oft genutzte Funktionen</p>
                 <div className='relative'>
                     <input id="message" rows="4" className={style.inputNm} placeholder="1 Tage (incl. 200km)" />
-                    <AiOutlineArrowRight className='absolute right-2 top-3' />
+                    <AiOutlineArrowRight className={style.span} />
                 </div>
                 <div className='relative'>
                     <input id="message" rows="4" className={style.inputNm} placeholder="rafzettel erhalten während der Anmietung" />
-                    <AiOutlineArrowRight className='absolute right-2 top-3' />
+                    <AiOutlineArrowRight className={style.span} />
                 </div>
             </div>
             <div className='mx-5 mt-5'>
-                <p className='text-[13px] font-[600]'>Nachrichten</p>
+                <p className={style.secondary}>Nachrichten</p>
                 <div className='bg-white rounded-lg shadow-lg mt-2 mx-5 p-4 flex items-center relative'>
                     <img className='w-[50px] h-[50px]' src={avater} />
                     <div className='ml-5'>
                         <p className='font-[900] '>Auto Name</p>
-                        <p className='text-[12px] text-[#AAAFB6] text-[400] flex items-center rounded-lg'>Hallo </p>
-                        <span className='flex items-center text-[#AAAFB6] text-[10px] font-[600]  absolute right-3 top-4'>18 März 18:40</span>
-                        <span className='flex items-center text-[black] text-[10px] font-[600]  absolute right-3 bottom-2'>Alle ansehen und antwortenc<AiOutlineArrowRight /></span>
+                        <p className={style.primary}>Hallo </p>
+                        <span className={`absolute right-3 text-[#AAAFB6] top-4 ${style.absolute}`}>18 März 18:40</span>
+                        <span className={`absolute right-3 text-[black] bottom-2 ${style.absolute}`}>Alle ansehen und antwortenc<AiOutlineArrowRight /></span>
                     </div>
                 </div>
             </div>
             <div className='mx-5 my-5'>
-                <p className='text-[13px] font-[600]'>Mieter</p>
+                <p className={style.secondary}>Mieter</p>
                 <div className='bg-[#7D7AFF] rounded-lg shadow-lg mt-2 mx-5 p-4 flex items-center relative'>
                     <img className='w-[50px] h-[50px]' src={avater} />
                     <div className='ml-5'>
