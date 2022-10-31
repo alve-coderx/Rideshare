f785b52b7bd2360863b52f89ca5e2887fff74c4c
alve-coderx <shadmanzz656@gmail.com>
2022-10-31 09:03:13 -0600

    bismillah

--- src/Pages/Entry/Entry.jsx
+++ src/Pages/Entry/Entry.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 import './style.css';
 const Entry = () => {
   const style = {
-    btn : 'border-t border-r border-b border-[white] text-[14px] font-[500] py-2 px-4 shadow w-[150px]'
+    btn : 'border-t border-r border-b border-[white] text-[14px] font-[500] py-4 px-4 shadow w-[150px]'
   }
   return (
     <div className='text-center flex flex-col items-center lg_bg ' style={{height : '100vh',backgroundRepeat : 'no-repeat',backgroundSize : 'cover',backgroundPosition : "center"}} >
-      <div className='pt-12 w-[306px]'>
-        <p className="text-[32px] font-[600] leading-[45px]	">Search, Book and Rent Your Vehincle Easily</p>
+      <div className='pt-12 w-[306px] lg:w-[600px]'>
+        <p className="text-[50px] font-[600] leading-[45px]	">Search, Book and Rent Your Vehincle Easily</p>
       </div>
       <div className='absolute bottom-14'>
             <Link to='/register'>
