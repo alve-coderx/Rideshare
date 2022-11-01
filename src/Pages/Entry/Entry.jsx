4b1400b074077d5c209abc1feab55806d5105258
alve-coderx <shadmanzz656@gmail.com>
2022-10-31 20:23:17 -0600

    bismillah

--- src/Pages/Entry/Entry.jsx
+++ src/Pages/Entry/Entry.jsx
@@ -3,22 +3,22 @@ import { Link } from 'react-router-dom';
 import './style.css';
 const Entry = () => {
   const style = {
-    btn : 'border-t border-r border-b border-[white] text-[14px] font-[500] py-4 px-4 shadow w-[150px]'
+    btn : 'border-t border-r border-b border-[white] text-[19px] font-[500] py-4 px-4 shadow w-[150px] font-[700]'
   }
   return (
     <div className='text-center flex flex-col items-center lg_bg ' style={{height : '100vh',backgroundRepeat : 'no-repeat',backgroundSize : 'cover',backgroundPosition : "center"}} >
       <div className='pt-12 w-[306px] lg:w-[600px]'>
-        <p className="lg:text-[50px] text-[40px] font-[600] leading-[45px]	">Search, Book and Rent Your Vehincle Easily</p>
+        <p className="lg:text-[50px] text-[40px] font-[600] leading-[60px]	">Search, Book and Rent Your Vehincle Easily</p>
       </div>
       <div className='absolute bottom-14'>
             <Link to='/register'>
-              <button className={`${style.btn} bg-[white] text-[#0E0F11] rounded-l-xl font-[700]`}>
+              <button className={`${style.btn} bg-[white] text-[#0E0F11] rounded-l-xl x`}>
                 Register
               </button>
             </Link>
             <Link to='/login'>
-              <button className={`${style.btn} bg-slate-700 text-white rounded-r-xl font-[700]`}>
-                Login
+              <button className={`${style.btn} bg-slate-700 text-white rounded-r-xl `}>
+                Log-in
               </button>
             </Link>
       </div>
