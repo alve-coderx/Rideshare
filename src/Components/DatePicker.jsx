cb2fa7e2721834732e7f4ee5c72b4ed2c659265a
alve-coderx <shadmanzz656@gmail.com>
2022-11-17 19:13:55 -0600

    bismillah

--- src/Components/DatePicker.jsx
+++ src/Components/DatePicker.jsx
@@ -3,7 +3,7 @@ import { MdDateRange } from 'react-icons/md';
 const DatePicker = ({color}) => {
   return (
     <div>
-        <input style={{backgroundColor : color}} className={` lg:text-[14px] md:text-[12px] text-[12px] font-[600] flex items-center outline-0 rounded-lg px-2 py-2 lg:w-52 md:w-52 w-44`} type='date'/>
+        <input style={{backgroundColor : color}} className={` lg:text-[14px] md:text-[12px] text-[12px] font-[600] flex items-center outline-0 rounded-lg lg:w-52 md:w-52 w-44`} type='datetime-local'/>
     </div>
   )
 }
